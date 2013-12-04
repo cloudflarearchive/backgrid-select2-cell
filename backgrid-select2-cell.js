85afb53c0118b077ad79163f277cb6c948b9fcd9
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-12-04 23:16:38 +0800

    Revert e86e9df4a066c2da5735e988aa804a47bf3fb0ae because select2 3.4.5 seems to have fixed their regression and the previous version is much simpler

--- backgrid-select2-cell.js
+++ backgrid-select2-cell.js
@@ -35,16 +35,12 @@
 
     /** @property */
     events: {
-      "change": "save",
-      "select2-opening": "toggleOpening",
-      "select2-open": "toggleOpen"
+      "close": "save",
+      "change": "save"
     },
 
     /** @property */
     select2Options: null,
-
-    /** @property */
-    opening: false,
 
     initialize: function () {
       Backgrid.SelectCellEditor.prototype.initialize.apply(this, arguments);
@@ -61,22 +57,6 @@
     },
 
     /**
-       Issues with the relatedTarget property on focusout / blur events in
-       Firefox require that we track the opening state of the select2 combo.
-     */
-    toggleOpening: function (e) {
-      this.opening = true;
-    },
-
-    /**
-       Issues with the relatedTarget property on focusout / blur events in
-       Firefox require that we track the opening state of the select2 combo.
-     */
-    toggleOpen: function (e) {
-      this.opening = false;
-    },
-
-    /**
        Renders a `select2` select box instead of the default `<select>` HTML
        element using the supplied options from #select2Options.
 
@@ -85,15 +65,7 @@
     render: function () {
       Backgrid.SelectCellEditor.prototype.render.apply(this, arguments);
       this.$el.select2(this.select2Options);
-      this.delegateEvents();
       return this;
-    },
-
-    /**
-       Returns the select2 container element
-    */
-    select2ContainerEl: function() {
-      return this.$el.parent().find("." + this.select2Options.containerCssClass);
     },
 
     /**
@@ -101,52 +73,21 @@
     */
     postRender: function () {
       var self = this;
-
-      this.select2ContainerEl()
+      this.$el.parent()
+        .find("." + this.select2Options.containerCssClass)
         .on("focusout", function (e) {
-          // Firefox behaves differently with regards to focusout
-          // @see https://github.com/wyuenho/backgrid/issues/247
-
-          var outside = e.currentTarget !== self.select2ContainerEl()[0];
-          if (!e.relatedTarget && self.opening === false && outside) {
-            self.close(e);
-          }
+          if (!e.relatedTarget) self.close(e);
         })
         .on("keydown", this.close)
         .attr("tabindex", -1).focus();
-    },
-
-    /**
-       Triggers a `backgrid:edited` event from the model so the body can close
-       this editor.
-    */
-    close: function (e) {
-      var model = this.model;
-      var column = this.column;
-      var Command = Backgrid.Command;
-      var command = new Command(e);
-      if (command.cancel()) {
-        e.stopPropagation();
-        model.trigger("backgrid:edited", model, column, new Command(e));
-      }
-      else if (command.save() || command.moveLeft() || command.moveRight() ||
-               command.moveUp() || command.moveDown() || e.type == "focusout") {
-        e.preventDefault();
-        e.stopPropagation();
-
-        if (e.type == "focusout" && this.$el.find("option").length === 1) {
-          model.set(column.get("name"), this.formatter.toRaw(this.$el.val(), model));
-        }
-        model.trigger("backgrid:edited", model, column, new Command(e));
-      }
     },
 
     remove: function () {
       this.$el.select2("destroy");
       return Backgrid.SelectCellEditor.prototype.remove.apply(this, arguments);
     }
-  });
 
+  });
 
   /**
      Select2Cell is a cell class that renders a `select2` select box during edit
